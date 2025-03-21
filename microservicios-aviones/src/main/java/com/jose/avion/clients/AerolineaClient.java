a1c94994ddb5b6114f4fb208849447bd3bb99cce
JoseSp <jose.salazarp_9612@hotmail.com>
2025-03-21 13:18:36 -0600

    cambio microservicio avion

--- microservicios-aviones/src/main/java/com/jose/avion/clients/AerolineaClient.java
+++ microservicios-aviones/src/main/java/com/jose/avion/clients/AerolineaClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.jose.commons.models.entities.Aerolinea;
 
-@FeignClient(name="microservicios-aerolineas")
+@FeignClient(name="microserviciosaerolineas")
 public interface AerolineaClient {
 	@GetMapping("/{id}")
 	public Optional<Aerolinea> getAerolineaById(@PathVariable Long id);
